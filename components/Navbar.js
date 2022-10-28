232116fce2ed69140c06d16dd0070df616d4f4e0
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-10-28 16:47:20 +0530

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,38 +1,193 @@
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+
+
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [shadow, setShadow] = useState(false);
+  const [navBg, setNavBg] = useState('black');
+  const [linkColor, setLinkColor] = useState('#1f2937');
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setShadow(true);
+      } else {
+        setShadow(false);
+      }
+    };
+    window.addEventListener('scroll', handleShadow);
+  }, []);
+
   return (
-    <div className="fixed w-full h-20 shadow-lg shadow-[#61dafb] z-[100]">
-      <div className="flex justify-between items-center w-full px-2 2xl:px-16">
-        {/* <Image class='px-5 py-5'src='/../public/images/logo.png' alt='/' width='200' height='50' py-20/> */}
+    <div
+      style={{ backgroundColor: `${navBg}` }}
+      className={
+        shadow
+          ? 'fixed w-full h-20 shadow-lg shadow-[#61dafb] z-[100] ease-in-out duration-300'
+          : 'fixed w-full shadow-lg shadow-[#61dafb] h-20 z-[100]'
+      }
+    >
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <Link href='/'>
         <h2 className="px-2 py-5">
           <span>&lt;</span>
           Kartikey/
           <span>&gt;</span>
         </h2>
-        <div >
-            <ul className="hidden md:flex">
-            <Link href='/'>
-                <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
-            </Link>
-            <Link href='/'>
-                <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
-            </Link>
-            <Link href='/'>
-                <li className="ml-10 text-sm uppercase hover:border-b">Project</li>
-            </Link>
-            <Link href='/'>
-                <li className="ml-10 text-sm uppercase hover:border-b">About</li>
-            </Link>
-            <Link href='/'>
-                <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
-            </Link>
-            </ul>
-            <div className="md:hidden">
-                <AiOutlineClose/>
+        </Link>
+        <div>
+          <ul style={{ color: `${linkColor}` }} className='hidden md:flex'>
+            <li className='ml-10 text-sm text-[#61dafb] uppercase hover:border-b'>
+              <Link href='/'>Home</Link>
+            </li>
+            <li className='ml-10 text-sm text-[#61dafb] uppercase hover:border-b'>
+              <Link href='/#about'>About</Link>
+            </li>
+            <li className='ml-10 text-sm text-[#61dafb] uppercase hover:border-b'>
+              <Link href='/#skills'>Skills</Link>
+            </li>
+            <li className='ml-10 text-sm text-[#61dafb] uppercase hover:border-b'>
+              <Link href='/#projects'>Projects</Link>
+            </li>
+            <li className='ml-10 text-sm text-[#61dafb] uppercase hover:border-b'>
+              <Link href='/resume'>Resume</Link>
+            </li>
+            <li className='ml-10 text-sm text-[#61dafb] uppercase hover:border-b'>
+              <Link href='/#contact'>Contact</Link>
+            </li>
+          </ul>
+          {/* Hamburger Icon */}
+          <div
+            style={{ color: `${linkColor}` }}
+            onClick={handleNav}
+            className='md:hidden'
+          >
+            <AiOutlineMenu size={25} color='#61dafb'/>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {/* Overlay */}
+      <div
+        className={
+          nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''
+        }
+      >
+        {/* Side Drawer Menu */}
+        <div
+          className={
+            nav
+              ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-black p-10 ease-in duration-500'
+              : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+          }
+        >
+          <div>
+            <div className='flex w-full items-center justify-between'>
+              <Link href='/'>
+              <h2 className="px-2 py-5">
+          <span>&lt;</span>
+          Kartikey/
+          <span>&gt;</span>
+        </h2>
+              </Link>
+              <div
+                onClick={handleNav}
+                className='rounded-full shadow-lg shadow-[#61dafb] p-3 cursor-pointer'
+              >
+                <AiOutlineClose />
+              </div>
             </div>
+            <div className='border-b border-gray-300 my-4'>
+              <p className='w-[85%] md:w-[90%] py-4'>
+                Let&#39;s build something legendary together
+              </p>
+            </div>
+          </div>
+          <div className='py-4 flex flex-col'>
+            <ul className='uppercase'>
+              <Link href='/'>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                  Home
+                </li>
+              </Link>
+              <Link href='/#about'>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                  About
+                </li>
+              </Link>
+              <Link href='/#skills'>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                  Skills
+                </li>
+              </Link>
+              <Link href='/#projects'>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                  Projects
+                </li>
+              </Link>
+              <Link href='/resume'>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                  Resume
+                </li>
+              </Link>
+              <Link href='/#contact'>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                  Contact
+                </li>
+              </Link>
+            </ul>
+            <div className='pt-10'>
+              <p className='uppercase tracking-widest text-[#61dafb]'>
+                Let&#39;s Connect
+              </p>
+              <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                <a
+                  href='https://www.linkedin.com/in/clint-briley-50056920a/'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <div className='rounded-full shadow-lg shadow-[#61dafb] p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaLinkedinIn />
+                  </div>
+                </a>
+                <a
+                  href='https://github.com/fireclint'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <div className='rounded-full shadow-lg shadow-[#61dafb] p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaGithub />
+                  </div>
+                </a>
+                <Link href='/#contact'>
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className='rounded-full shadow-lg shadow-[#61dafb] p-3 cursor-pointer hover:scale-105 ease-in duration-300'
+                  >
+                    <AiOutlineMail />
+                  </div>
+                </Link>
+                <Link href='/resume'>
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className='rounded-full shadow-lg shadow-[#61dafb] p-3 cursor-pointer hover:scale-105 ease-in duration-300'
+                  >
+                    <BsFillPersonLinesFill />
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
