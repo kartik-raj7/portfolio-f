954fde91bd4d1a19df42cce2b98cf941df9d2391
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-10-30 20:59:37 +0530

    update

--- components/Contact.js
+++ components/Contact.js
@@ -67,7 +67,7 @@ const Contact = () => {
           <div className='col-span-3 w-full h-auto shadow-xl shadow-[#61dafb] rounded-xl lg:p-4'>
             <div className='p-4'>
               <form
-                action='https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060'
+                action='https://getform.io/f/d3e32b1e-29b0-499c-b79c-d3ad13b52c7f'
                 method='POST'
                 encType='multipart/form-data'
               >
